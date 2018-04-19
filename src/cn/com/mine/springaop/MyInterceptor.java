1a55db61ed480a55bb73b61e1c8f5d3e568ad5ab
RayGaby <1175713423@qq.com>
2018-04-19 14:14:56 +0800

    update

--- src/cn/com/mine/springaop/MyInterceptor.java
+++ src/cn/com/mine/springaop/MyInterceptor.java
@@ -17,22 +17,22 @@ public class MyInterceptor {
     // 声明该方法是一个前置通知：在目标方法开始之前执行 
     @Before("anyMethod()")
     public void doAccessCheck() {
-        System.out.println("前置通知");
+        System.out.println("前置通知@Before");
     }
 
     @AfterReturning("anyMethod()")
     public void doAfterReturning() {
-        System.out.println("后置通知");
+        System.out.println("后置通知@AfterReturning");
     }
 
     @After("anyMethod()")
     public void doAfter() {
-        System.out.println("最终通知");
+        System.out.println("最终通知@After");
     }
 
     @AfterThrowing("anyMethod()")
     public void doAfterThrowing() {
-        System.out.println("异常通知");
+        System.out.println("异常通知@AfterThrowing");
     }
 
     @Around("anyMethod()")
